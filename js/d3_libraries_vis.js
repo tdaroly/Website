c7857d9489357166d4ba490cd1f8e2485f6a5ba1
Taher Daroly <41846928+tdaroly@users.noreply.github.com>
2019-01-09 17:17:24 -0600

    Add files via upload

---
+++ js/d3_libraries_vis.js
@@ -0,0 +1,155 @@
+
+// http://blog.thomsonreuters.com/index.php/mobile-patent-suits-graphic-of-the-day/
+var links = [
+  {source: "Microsoft", target: "Amazon", type: "licensing"},
+  {source: "Microsoft", target: "HTC", type: "licensing"},
+  {source: "Samsung", target: "Apple", type: "suit"},
+  {source: "Motorola", target: "Apple", type: "suit"},
+  {source: "Nokia", target: "Apple", type: "resolved"},
+  {source: "HTC", target: "Apple", type: "suit"},
+  {source: "Kodak", target: "Apple", type: "suit"},
+  {source: "Microsoft", target: "Barnes & Noble", type: "suit"},
+  {source: "Microsoft", target: "Foxconn", type: "suit"},
+  {source: "Oracle", target: "Google", type: "suit"},
+  {source: "Apple", target: "HTC", type: "suit"},
+  {source: "Microsoft", target: "Inventec", type: "suit"},
+  {source: "Samsung", target: "Kodak", type: "resolved"},
+  {source: "LG", target: "Kodak", type: "resolved"},
+  {source: "RIM", target: "Kodak", type: "suit"},
+  {source: "Sony", target: "LG", type: "suit"},
+  {source: "Kodak", target: "LG", type: "resolved"},
+  {source: "Apple", target: "Nokia", type: "resolved"},
+  {source: "Qualcomm", target: "Nokia", type: "resolved"},
+  {source: "Apple", target: "Motorola", type: "suit"},
+  {source: "Microsoft", target: "Motorola", type: "suit"},
+  {source: "Motorola", target: "Microsoft", type: "suit"},
+  {source: "Huawei", target: "ZTE", type: "suit"},
+  {source: "Ericsson", target: "ZTE", type: "suit"},
+  {source: "Kodak", target: "Samsung", type: "resolved"},
+  {source: "Apple", target: "Samsung", type: "suit"},
+  {source: "Kodak", target: "RIM", type: "suit"},
+  {source: "Nokia", target: "Qualcomm", type: "suit"}
+];
+
+
+
+
+var links = [
+
+  {source: "pandas", target: "Python", type: "libraries_python"},
+  {source: "NumPy", target: "Python", type: "libraries_python"},
+  {source: "Flask", target: "Python", type: "libraries_python"},
+  {source: "nltk", target: "Python", type: "libraries_python"},
+  {source: "SQLAlchemy", target: "Python", type: "libraries_python"},
+  {source: "Plotly", target: "Python", type: "libraries_python"},
+  {source: "Django", target: "Python", type: "libraries_python"},
+  {source: "SciPy", target: "Python", type: "libraries_python"},
+  {source: "matplotlib", target: "Python", type: "libraries_python"},
+  {source: "requests", target: "Python", type: "libraries_python"},
+  {source: "scikit-learn", target: "Python", type: "libraries_python"},
+  {source: "leaflet", target: "Python", type: "libraries_python"},
+  {source: "Plotly", target: "Javascript", type: "common_libs"},
+  {source: "Javascript", target: "Plotly", type: "common_libs"},
+  {source: "arcgis.js", target: "Javascript", type: "libraries_javascript"},
+  {source: "D3.js", target: "Javascript", type: "libraries_javascript"},
+  {source: "node.js", target: "Javascript", type: "libraries_javascript"},
+  {source: "Google.js(plotting)", target: "Javascript", type: "libraries_javascript"},
+  {source: "Swing", target: "Java", type: "libraries_java"},
+  {source: "Util", target: "Java", type: "libraries_java"},
+  {source: "awt", target: "Java", type: "libraries_java"},
+  {source: "JFrame", target: "Java", type: "libraries_java"},
+  {source: "JButton", target: "Java", type: "libraries_java"},
+  {source: "math.h", target: "C/C++", type: "c_libraries"},
+  {source: "limits.h", target: "C/C++", type: "c_libraries"},
+  {source: "string.h", target: "C/C++", type: "c_libraries"},
+  {source: "stack", target: "C/C++", type: "c_libraries"},
+  {source: "deque", target: "C/C++", type: "c_libraries"},
+  {source: "list", target: "C/C++", type: "c_libraries"},
+  {source: "vector", target: "C/C++", type: "c_libraries"},
+  {source: "Plotly", target: "R", type: "common_libs"},
+  {source: "R", target: "Plotly", type: "common_libs"},
+  {source: "R", target: "leaflet", type: "common_libs"},
+  {source: "leaflet", target: "R", type: "common_libs"},
+  {source: "data.frame", target: "R", type: "R_libraries"}
+
+];
+
+
+
+
+
+
+var nodes = {};
+
+// Compute the distinct nodes from the links.
+links.forEach(function(link) {
+  link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+  link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+});
+
+var width = 960,
+    height = 500;
+
+var force = d3.layout.force()
+    .nodes(d3.values(nodes))
+    .links(links)
+    .size([width, height])
+    .linkDistance(60)
+    .charge(-300)
+    .on("tick", tick)
+    .start();
+
+var svg = d3.select(".black").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+// Per-type markers, as they don't inherit styles.
+svg.append("defs").selectAll("marker")
+    .data(["libraries_python","libraries_javascript","common_libs","libraries_java","c_libraries","R_libraries"])
+  .enter().append("marker")
+    .attr("id", function(d) { return d; })
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 15)
+    .attr("refY", -1.5)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("orient", "auto")
+  .append("path")
+    .attr("d", "M0,-5L10,0L0,5");
+
+var path = svg.append("g").selectAll("path")
+    .data(force.links())
+  .enter().append("path")
+    .attr("class", function(d) { return "link " + d.type; })
+    .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
+
+var circle = svg.append("g").selectAll("circle")
+    .data(force.nodes())
+  .enter().append("circle")
+    .attr("r", 9)
+    .call(force.drag);
+
+var text = svg.append("g").selectAll("text")
+    .data(force.nodes())
+  .enter().append("text")
+    .attr("x", 8)
+    .attr("y", ".31em")
+    .text(function(d) { return d.name; });
+
+// Use elliptical arc path segments to doubly-encode directionality.
+function tick() {
+  path.attr("d", linkArc);
+  circle.attr("transform", transform);
+  text.attr("transform", transform);
+}
+
+function linkArc(d) {
+  var dx = d.target.x - d.source.x,
+      dy = d.target.y - d.source.y,
+      dr = Math.sqrt(dx * dx + dy * dy);
+  return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+}
+
+function transform(d) {
+  return "translate(" + d.x + "," + d.y + ")";
+}
